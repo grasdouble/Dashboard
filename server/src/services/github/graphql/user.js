c2767d23e7a949c701900db9709918295afdef1b
smouillour <smouillour@talend.com>
2019-04-20 00:11:04 +0200

    init list of potential query

---
+++ server/src/services/github/graphql/user.js
@@ -0,0 +1,8 @@
+export const userInformation = () => {};
+export const listOfRepositories = () => {};
+export const listOfProjects = () => {};
+export const listOfOrganizations = () => {};
+export const listOfStar = () => {};
+export const listOfFollowers = () => {};
+export const listOfFollowing = () => {};
+export const listOfNotification = () => {};
