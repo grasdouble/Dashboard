512632096c5f5310dff8e12ff2b8481c5b26a8fa
smouillour <smouillour@talend.com>
2019-04-27 01:50:28 +0200

    test

--- .travis/before-addTokenServer.sh
+++ .travis/before-addTokenServer.sh
@@ -4,4 +4,5 @@ cd "$TRAVIS_BUILD_DIR"
 echo "export const github = {token: '${TEST}'};" > packages/server/src/config.js
 echo "DISPLAY CONFIG"
 echo "--------------"
+pwd
 tail packages/server/src/config.js
