1502439be51f3180669e7cc86b8dc0b47b208ce0
smouillour <smouillour@talend.com>
2019-04-27 01:59:30 +0200

    revert some changes

--- .travis/before-addTokenServer.sh
+++ .travis/before-addTokenServer.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd "$TRAVIS_BUILD_DIR"
-echo "export const github = {token: '${TEST}'};" > packages/server/src/config.js
-echo "DISPLAY CONFIG"
-echo "--------------"
-pwd
-tail packages/server/src/config.js
+echo "export const github = {token: '${TOKEN}'};" > packages/server/src/config.js
