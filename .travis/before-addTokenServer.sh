45f97fbeb6f56c87f735c46a3c5840670238219e
smouillour <smouillour@talend.com>
2019-04-27 02:26:34 +0200

    Oo

--- .travis/before-addTokenServer.sh
+++ .travis/before-addTokenServer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd "$TRAVIS_BUILD_DIR"
-echo "export const github = { token: '${TOKEN}' };" > packages/server/src/config.js
+echo "export const github = { token: '$TOKEN' };" > packages/server/src/config.js
