fa01472c6b9c83df54d003b963c4ee230487506b
smouillour <smouillour@talend.com>
2019-05-07 09:30:29 +0200

    add duplicate code

--- packages/backend/src/services/github/graphql/project.js
+++ packages/backend/src/services/github/graphql/project.js
@@ -16,3 +16,28 @@ export const projectInfo = (owner, repository, projectNumber, customQuery) => {
         }
     }`;
 };
+
+/**
+ * Duplicated code
+ * @param {string} login
+ */
+export const userInformations = login => `query {
+    user(login: "${login}") {
+      login
+      name
+      email
+      company
+      bio
+      avatarUrl
+      createdAt
+      watching {
+        totalCount
+      }
+      following {
+        totalCount
+      }
+      followers {
+        totalCount
+      }
+    }
+  }`;
